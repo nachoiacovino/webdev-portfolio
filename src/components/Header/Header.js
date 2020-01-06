30302983ffb6730c8ccb867f4f6a662582bf58df
Ignacio Iacovino <nacho.iacovino@gmail.com>
2020-01-06 22:36:59 +0100

    Optimize images and header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -6,7 +6,7 @@ const Header = () => {
         <header className="masthead bg-primary text-white text-center" id="page-top">
             <div className="container d-flex align-items-center flex-column">
             {/* Masthead Avatar Image */}
-            <img className="masthead-avatar mb-5" src="img/avatar.png" alt="Avatar" />
+            <img className="masthead-avatar mb-5" src="img/avatar.png" alt="Avatar" width={240} height={240} />
             {/* Masthead Heading */}
             <h1 className="masthead-heading text-uppercase mb-0">Ignacio Iacovino</h1>
             {/* Icon Divider */}
